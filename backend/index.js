3d1e5cf7f0c20182f268023dd9bf5bfe2da372c5
Vignesh Murugan <vicky.web3@gmail.com>
2024-01-25 18:45:08 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,14 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
-// const db = require("./db.js");
-const mainRouter = require("./routes/index.js");
-
 const app = express();
 const port = 4000;
 
 // global middleares
 app.use(cors());
 app.use(express.json());
+
+const mainRouter = require("./routes/index.js");
 
 app.use("/api/v1", mainRouter);  
         
